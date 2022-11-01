394413f907d95178ee7df00bb8c0b34b9b09759c
lemuellloren <lempakz@yahoo.com>
2022-11-01 20:46:50 +0800

    Update contents

--- data/config.js
+++ data/config.js
@@ -1,10 +1,11 @@
 export const meta = {
-  title: 'LeBron James - Web Developer and Designer',
-  description: 'Hey, I\'m LeBron. I\'m a web developer and designer building things people love.'
-}
+  title: 'Lemuel Lloren - Web Developer and Designer',
+  description:
+    "Hey, I'm Lemuel Lloren. I'm a web developer and designer building things people love."
+};
 
 export const hero = {
-  title: "Hey there! I'm LeBron James.",
+  title: "Hey there! I'm Lemuel Lloren.",
   desc: "I'm a web developer and designer building things people love."
 };
 
@@ -13,22 +14,46 @@ export const projects = {
   desc: "Here are some of my favorite projects that I've worked on.",
   projects: [
     {
-      title: 'Lakers Official',
-      description: 'Home to the Los-Angeles Lakers',
-      link: 'https://lakers.com',
+      title: 'Poplar Homes Engineering Careers',
+      description:
+        "Poplar's microsite for engineering department careers made with WordPress",
+      link: 'https://www.poplarhomes.com/engineeringcareers/',
       github: ''
     },
     {
-      title: 'Cool App',
-      description: 'A cool app!',
-      link: 'https://lebronscoolapp.com',
-      github: 'theking-lbj/cool-app'
+      title: 'Poplar Homes ROI Calculator',
+      description:
+        'A ROI Calculor page for property management that shows how much time and money owner can save with Poplar Homes’ tech-enabled services made with JavaScript and WordPress',
+      link: 'https://www.poplarhomes.com/roi-calculator/',
+      github: ''
     },
     {
-      title: 'Another app',
-      description: 'Another app!',
-      link: 'https://anotherlebronapp.com',
-      github: 'theking-lbj/another-app'
+      title: 'ViewQwest Broadband',
+      description:
+        'ViewQwest broadband sign up portal made with WordPress, VueJS, and Laravel',
+      link: 'https://signup.viewqwest.com/broadband/',
+      github: ''
+    },
+    {
+      title: 'ViewQwest Pte Ltd',
+      description:
+        'ViewQwest residential and corporate website made with WordPress using Elementor',
+      link: 'https://viewqwest.com/',
+      github: ''
+    },
+    {
+      title: 'First Future 2.0',
+      description:
+        'First Future 2.0 is a workforce development project implemented by PBEd, funded by the Citi Foundation as part of its Pathways to Progress focus area made with Moodle and Javascript',
+      link: 'https://firstfutureph.org/',
+      github: ''
+    },
+    {
+      title: 'Responsible Pet Ownership',
+      description:
+        'An eLearning site supported by the Office of Local Government Grants Program made with Kirby CMS, UIKit, and PHP',
+      link: 'https://www.penrithpeteducation.com.au/',
+      github: ''
     }
   ]
 };
@@ -41,17 +66,20 @@ export const stack = {
     'JavaScript',
     'Typescript',
     'React',
-    'NextJS',
+    'VueJS',
+    'NodeJS',
+    'GraphQL',
     'TailwindCSS',
-    'Firebase',
+    'PHP',
+    'WordPress',
     'Git'
   ]
 };
 
 export const contact = {
   title: 'Contact Me',
-  email: 'lebron@gmail.com',
-  github: 'theking-lbj',
-  dribbble: 'theking-lbj',
-  linkedin: 'theking-lbj'
+  email: 'lemuelpaconlloren@gmail.com',
+  github: 'lemuellloren',
+  dribbble: 'Lemuel',
+  linkedin: 'lemuel-lloren'
 };
