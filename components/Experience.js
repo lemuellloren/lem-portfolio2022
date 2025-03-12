8900339ba3adfb501fb192f02ef28867a3ac55b9
lemuellloren <lempakz@yahoo.com>
2025-03-12 18:24:27 +0800

    Integrate AnimatedContent component into Hero, Experience, and Contact sections for enhanced animations; add smooth scrolling and overflow-x hidden to global styles

--- components/Experience.js
+++ components/Experience.js
@@ -1,7 +1,8 @@
 'use client';
-import { experience } from '@/data/config';
+import { defaultAnimationConfig, experience } from '@/data/config';
 import { motion, useSpring } from 'framer-motion';
 import React, { useState, useRef } from 'react';
+import AnimatedContent from './animated/AnimatedContent';
 
 export const Experience = () => {
   const [img, setImg] = useState({
@@ -35,55 +36,57 @@ export const Experience = () => {
   };
   return (
     <section>
-      <h2 className="md:mb-40 lowercase text-2xl md:text-9xl	font-normal text-right">
-        {experience.title}
-      </h2>
-      <div
-        ref={containerRef}
-        onMouseMove={handleMove}
-        className="relative w-full py-8"
-      >
-        {experience.experiences.map((experience) => (
-          <div
-            key={experience.company}
-            onMouseEnter={() => handleImageInteraction(experience, 1)}
-            onMouseMove={() => handleImageInteraction(experience, 1)}
-            onMouseLeave={() => handleImageInteraction(experience, 0)}
-            className="w-full py-14 cursor-pointer text-center  text-white  border-b border-dark dark:border-white last:border-none"
-          >
-            <a
-              href={experience.link}
-              target="_blank"
-              rel="noreferrer"
-              className="flex justify-between"
+      <AnimatedContent {...defaultAnimationConfig}>
+        <h2 className="md:mb-40 lowercase text-2xl md:text-9xl	font-normal text-right">
+          {experience.title}
+        </h2>
+        <div
+          ref={containerRef}
+          onMouseMove={handleMove}
+          className="relative w-full py-8"
+        >
+          {experience.experiences.map((experience) => (
+            <div
+              key={experience.company}
+              onMouseEnter={() => handleImageInteraction(experience, 1)}
+              onMouseMove={() => handleImageInteraction(experience, 1)}
+              onMouseLeave={() => handleImageInteraction(experience, 0)}
+              className="w-full py-14 cursor-pointer text-center  text-white  border-b border-dark dark:border-white last:border-none"
             >
-              <div className="text-left">
-                <h2 className="text-xs md:text-7xl font-normal">
-                  {experience.company}
-                </h2>
-                <span className="text-xs md:text-lg font-light">
-                  {experience.position}
+              <a
+                href={experience.link}
+                target="_blank"
+                rel="noreferrer"
+                className="flex justify-between"
+              >
+                <div className="text-left">
+                  <h2 className="text-xs md:text-7xl font-normal">
+                    {experience.company}
+                  </h2>
+                  <span className="text-xs md:text-lg font-light">
+                    {experience.position}
+                  </span>
+                </div>
+                <span className="text-xs md:text-sm font-normal">
+                  {experience.year}
                 </span>
-              </div>
-              <span className="text-xs md:text-sm font-normal">
-                {experience.year}
-              </span>
-            </a>
-          </div>
-        ))}
+              </a>
+            </div>
+          ))}
 
-        <motion.img
-          ref={imageRef}
-          src={img.src}
-          alt={img.alt}
-          className="w-14 h-14 object-cover absolute top-0 left-0 transition-opacity duration-200 ease-in-out pointer-events-none"
-          style={{
-            x: imagePos.x,
-            y: imagePos.y,
-            opacity: img.opacity
-          }}
-        />
-      </div>
+          <motion.img
+            ref={imageRef}
+            src={img.src}
+            alt={img.alt}
+            className="w-14 h-14 object-cover absolute top-0 left-0 transition-opacity duration-200 ease-in-out pointer-events-none"
+            style={{
+              x: imagePos.x,
+              y: imagePos.y,
+              opacity: img.opacity
+            }}
+          />
+        </div>
+      </AnimatedContent>
     </section>
   );
 };
