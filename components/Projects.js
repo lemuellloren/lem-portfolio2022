be4ea298b48208da3d226e37fd145dd7578ca91d
lemuellloren <lempakz@yahoo.com>
2025-03-17 10:09:36 +0800

    Refactor Projects component to dynamically render Double components from project array, improving code readability and maintainability

--- components/Projects.js
+++ components/Projects.js
@@ -11,27 +11,20 @@ export const Projects = () => {
         velocity={100}
         className="lowercase text-2xl md:text-9xl font-normal p-2"
       />
+
       <div>
-        <Double projects={[projects.projects[0], projects.projects[1]]} />
-        <Double
-          projects={[projects.projects[2], projects.projects[3]]}
-          reversed={true}
-        />
-        <Double projects={[projects.projects[4], projects.projects[5]]} />
-        <Double
-          projects={[projects.projects[6], projects.projects[7]]}
-          reversed={true}
-        />
-        <Double projects={[projects.projects[8], projects.projects[9]]} />
-        <Double
-          projects={[projects.projects[10], projects.projects[11]]}
-          reversed={true}
-        />
-        <Double projects={[projects.projects[12], projects.projects[13]]} />
-        <Double
-          projects={[projects.projects[14], projects.projects[15]]}
-          reversed={true}
-        />
+        {projects.projects.map((_, index, arr) => {
+          if (index % 2 === 0 && arr[index + 1]) {
+            return (
+              <Double
+                key={index}
+                projects={[arr[index], arr[index + 1]]}
+                reversed={index % 4 !== 0}
+              />
+            );
+          }
+          return null;
+        })}
       </div>
     </section>
   );
